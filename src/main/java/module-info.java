42dea6a89d298a377cba535202a8aa8e74ddeda4
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2024-09-03 17:07:08 +0200

    [FIX] Add missing provider.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -32,4 +32,6 @@ module be.yildizgames.module.controller.sdl {
     requires be.yildizgames.module.controller;
 
     exports be.yildizgames.module.controller.sdl;
+
+    provides be.yildizgames.module.controller.ControllerEngine with be.yildizgames.module.controller.sdl.SdlControllerEngine;
 }
